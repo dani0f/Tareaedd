70dc75ee86459d1b063c46cc06f981a239029519
Dani <daniela.moreno1@mail.udp.cl>
2018-06-08 23:20:44 -0400

    Eliminar contacto abb

--- arbolbinario.py
+++ arbolbinario.py
@@ -4,11 +4,11 @@ class Nodo_ab:
 		self.ape=ape
 		self.tel=tel
 		self.mail=mail
-		#self.next=None
-		#self.prev=None
 		self.right=None
 		self.left=None
 		self.parent=None
+	def get_parent(self):
+		return(self.parent)
 	def get_nom(self):
 		return(self.nom)
 	def get_ape(self):
@@ -94,9 +94,53 @@ class Arbol_b():
 		else:
 			print(con.get_nom(),con.get_ape(),con.get_tel(),con.get_mail())
 			return(True)
+
+	def delete(self,nom,ape):
+		if self.empty():
+			return(None)
+		else:
+			return self._delete(self.find(nom,ape))
+	def _delete(self,node):
+		def min_nombre(nodo):
+			pos=nodo
+			while(pos.left != None):
+				pos=pos.left
+			return (pos)
+		def n_hijos(nodo):
+			n_hijos=0
+			if(nodo.left != None):
+				n_hijos+=1
+			if(nodo.right != None):
+				n_hijos+=1
+			return (n_hijos)
+		node_parent = node.parent
+		node_children = n_hijos(node)
+		if node_children == 0:
+			print("Sin hijos")
+			if node_parent.left == node:
+				node_parent.left = None
+			else:
+				node_parent.right = None
+		if node_children == 1:
+			print("Un hijo")
+			if node.left != None:
+				child = node.left
+			else:
+				child = node.right
+			if node_parent.left == node:
+				node_parent.left = child
+			else:
+				node_parent.right = child
+			child.parent = node_parent
+		if node_children == 2:
+			print("Dos hijos")
+			successor = min_nombre(node.right) #sucesor in order del que sera eliminado 
+			node.nom = successor.nom
+			node.ape = successor.ape  
+			self._delete(successor)
 arbol=Arbol_b()
 arbol.add_1("d","moreno",78743,"dani@mail.com")
-arbol.add_1("a","morenoSeaT:V→Wuna  aplicaci  ́on  lineal  y  sea{v1,  .  .  .  ,  vn}  ⊂Vun  conjunto  de  vectorestales  que{T(v1),  .  .  .  ,  T(vn)}es  linealmente  independiente.  Demuestre  que{v1,  .  .  .  ,  vn}es  linealmente  independiente",75432,"dasdi@mail.com")
+arbol.add_1("a","moreno",75432,"dasdi@mail.com")
 arbol.add_1("n","moreno",7543,"dasdi@mail.com")
 arbol.add_1("i","moreno",787323,"dafdi@mail.com")
 arbol.in_order(arbol.get_root())
@@ -104,5 +148,7 @@ print("-------------------------")
 #arbol.pre_order(arbol.get_root())
 #print("-------------------------")
 #arbol.pos_order(arbol.get_root())
-retorno=(arbol.find("d","moreno"))
-arbol.print_contacto("d","moreno")
+#retorno=(arbol.find("d","moreno"))
+#arbol.print_contacto("d","moreno")
+arbol.delete("d","moreno")
+arbol.in_order(arbol.get_root())
