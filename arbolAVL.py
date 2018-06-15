5d3bba60b40997d551437c36d9e322bdabdf94be
Dani <daniela.moreno1@mail.udp.cl>
2018-06-15 00:06:11 -0400

    Agregando metodo add _add y rotacion a R

--- arbolAVL.py
+++ arbolAVL.py
@@ -85,13 +85,48 @@ class Nodo:
 class Avl:
 	def __init__(self):
 		self.root=None
+	def empty():
+		if(self.root==None):
+			return(True)
+		return(False)
+	def add(self,dato):
+		if(self.root is None):
+			nodo=Nodo(dato)
+			self.root=nodo
+		else:	
+			self._add(dato,self.root)
+	def add(self,dato):
+		if(self.empty()):
+			return(False)
+		else:
+			self._add(dato,self.root)
+			self.in_order_balanced(self.root)
+			nodo=self._balanceo(self.root)
+			if(nodo==None):
+				return(False)
+			else:
+				self._rotacion(nodo)#mando a rotar
+				return(True)
+	def _add(self,dato,nodo):
+		if(dato<nodo.get_dato()):
+			if(nodo.left!=None):
+				self.add(dato,nodo.left)
+			else:
+				nodo.left=Nodo(dato)
+				nodo.left.parent=nodo
+		else:
+			if(hoja.right!=None):
+				self.add(dato,hoja.right)
+			else:
+				hoja.right=Hoja(dato)
+				hoja.right.parent=hoja
 	def _balanceo(self,nodo):
 		if(nodo==None):
 			return(None)
 		else:
 			factor=nodo.get_factor()
 			if(factor==2 or factor==-2):
-				print(factor,nodo.get_nombre())
+				#print(factor,nodo.get_nombre())
 				return(nodo)
 			else:
 				a=self._balanceo(nodo.left)
@@ -108,25 +143,36 @@ class Avl:
 			self.in_order_balanced(nodo.left)
 			nodo._factor()
 			self.in_order_balanced(nodo.right)
-	def tipo_rotacion(self,nodo):
+	def _rotacion(self,nodo):
 		if(nodo.get_factor()==-2):
 			nod=nodo.right.get_factor()
 			if(nod==-1):
+				self.rotacion_L(nodo,nodo.get_parent())
 				return("L")
 			if(nod==1):
+				self.rotacion_RL(nodo,nodo.get_parent())
 				return("RL")
 		if(nodo.get_factor()==2):
 			nod=nodo.left.get_factor()
 			if(nod==-1):
+				self.rotacion_LR(nodo,nodo.get_parent())
 				return("LR")
 			if(nod==1):
+				self.rotacion_R(nodo,nodo.get_parent())
 				return("R")
+	def pre_order(self,nodo):
+		if nodo==None:
+			pass
+		else:
+			print(nodo.get_nombre())
+			self.pre_order(nodo.left)
+			self.pre_order(nodo.right)
 	def in_order(self,nodo):
 		if nodo==None:
 			pass
 		else:
 			self.in_order(nodo.left)
-			print(nodo.get_nom(),nodo.get_ape(),nodo.get_tel(),nodo.get_mail())
+			#print(nodo.get_nombre())
 			self.in_order(nodo.right)
 	def balanceo(self):
 		if(self.root is None):
@@ -143,30 +189,43 @@ class Avl:
 			return False
 		ver=self._balanceo(nodo)
 		if(ver==None):
-			print("False")
 			return(False)
 		else:
-			print("true")
+			if(ver.right is not None):
+				if(ver.right.get_factor()==2 or ver.right.get_factor()==-2):
+					ver=ver.right
+			if(ver.left is not None):
+				if(ver.left.get_factor()==2 or ver.left.get_factor()==-2):
+					ver=ver.left
 			return(ver)
-	def rotacion_l(self,nodo):
-		B=nodo
-		A=B.parent
-		C=B.right
-		D=C.right
-		A.right=C
-		C.left=B
-		C.right=D
-		C.parent=A
-		B.parent=C
-		D.parent=c			
-	def rotacion_r(self,nodo,parent):
-		B=nodo
+
+	def rotacion_L(self,nodo,parent):
+		print("rotacion a L")
+		pass	
+	def rotacion_R(self,nodo,parent):
+		print("rotacion a R")
+		B=nodo 
 		A=parent
 		C=B.left
-		D=C.left
-		A.right=C
+		B.left=None
 		C.right=B
-		C.left=D
+		#caso 0 b es una raiz y no tiene parent
+		if(A is None):
+			print(" se cambia la raiz")
+		#caso 1 p.factor<0 
+		if(A.get_factor()<0):
+			A.right=C
+			print("caso 1")
+		#caso 2 p.factor>0 
+		if(A.get_factor()>0):
+			A.left=C
+			print("caso 2")
+		C.parent=A
+		B.parent=C
+	def rotacion_LR(self,nodo,parent):
+		print("rotacion a LR")
+	def rotacion_RL(self,nodo,parent):
+		print("rotacion a RL")
 A=Nodo("A")
 B1=Nodo("B1")
 B=Nodo("B")
@@ -176,16 +235,21 @@ C=Nodo("C")
 D=Nodo("D")
 A.right=B
 A.left=B1
-B.left=C
+B1.left=C
 C.left=D
-B1.left=C1
-B1.right=C2
+B.parent=A
+B1.parent=A
+C.parent=B1
+D.parent=C
 avl=Avl()
 avl.in_order_balanced(A)
 ver=avl.balanceo_1(A)
-print(avl.tipo_rotacion(ver))
-avl.rotacion_r(ver,A)
-avl.in_order(A)
+print("Nodo desbalanceado encontrado",ver.get_nombre())
+avl.pre_order(A)
+print("----------")
+print("Aplicando rotacion tipo",avl._rotacion(ver))
+avl.pre_order(A)
+
 
 
 
