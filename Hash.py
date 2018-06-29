c2454a68cdf10d506668e90a87fe98eee4539a50
Dani <daniela.moreno1@mail.udp.cl>
2018-06-29 17:59:40 -0400

    hash

--- Hash.py
+++ Hash.py
@@ -1,24 +1,26 @@
+from faker import Faker
 from listadoble import Lista_s
 from listadoble import Nodo
+from time import time
 class Hash:
   def __init__(self,size):
     self.list = [None]*size
     self.size= size
-    self.arr=[]
+    self.arr=Lista_s()
   def str2num(self,key):
     return sum([ord(c) for c in key])
   def hashstr(self,key,size):
     return self.str2num(key)%size
   def put(self,nom,ape,tel,mail):
-    self.arr.append(ape)
+    self.arr._insertar(nom,ape,tel,mail)
     pos = self.hashstr(ape,self.size)
     if self.list[pos] is not None:
       #Si existe esa cola solo la agrego ahi
-      print(self.list[pos]._insertar(nom,ape,tel,mail))
+      self.list[pos]._insertar(nom,ape,tel,mail)
     else:
       #Si no existe creo una cola y lo agrego
       self.list[pos]=Lista_s()
-      print(self.list[pos]._insertar(nom,ape,tel,mail))
+      self.list[pos]._insertar(nom,ape,tel,mail)
   def H_buscar(self,ape):
     pos = self.hashstr(ape,self.size)
     if self.list[pos] is not None:#si existe
@@ -36,6 +38,7 @@ class Hash:
     if(self.H_buscar(ape) is None):
       return("inexistente")
     else:
+      self.arr.eliminar(ape)
       pos = self.hashstr(ape,self.size)
       if self.list[pos] is not None:#si existe
         aux=self.list[pos]
@@ -44,13 +47,6 @@ class Hash:
       else:
         return(False)
   def H_imprimir_todo(self):
-    self.arr.sort()
-    for i in range(len(self.arr)):
-      print(self.arr[i])
-    return
-hashi=Hash(100)
-hashi.put("marlin","lenzi",43242,"mail.com")
-hashi.put("romina","urrutia",43242,"mail.com")
-hashi.put("levi","leni",43242,"mail.com")
-hashi.H_imprimir_todo()
-#print("Hash importado")
+    self.arr.imprimir_todos()
+
+
